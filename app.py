56f046104593442c4497e11f8ef7bc40934ac181
Brandonnogithub <2275135452@qq.com>
2020-06-17 13:49:32 +0800

    add args to app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
+import argparse
 from urllib import parse
-
 from flask import Flask, request, render_template
 
 # init flask app and env variables
@@ -120,4 +120,12 @@ def truncate_url(url):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--local", "-l", action='store_true', help="run local")
+    parser.add_argument("--port", "-p", default=5000, type=int, help="runnning port")
+    args = parser.parse_args()
+
+    if args.local:
+        app.run(host='127.0.0.1', port=args.port)
+    else:
+        app.run(host='0.0.0.0', port=args.port)
